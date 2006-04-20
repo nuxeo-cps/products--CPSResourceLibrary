1cf15d2bc2a17f47fd6a982ecdd37afed438e89a
tziade <devnull@localhost>
2006-04-20 14:52:42 +0000

    added header

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,21 @@
+# -*- coding: ISO-8859-15 -*-
+# Copyright (c) 2006 Nuxeo SARL <http://nuxeo.com>
+# Authors: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
 from _testbrowser import Browser
 
